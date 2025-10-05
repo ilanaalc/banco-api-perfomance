e4d8d38e2621bfa42ed2bc4df10e6ceb7d6cc42c
Ilana Alcantara <ilanaalcantara4@gmail.com>
2025-10-05 00:20:15 -0300

    Adicionei VUs e Duration para execução do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -5,9 +5,10 @@ import { sleep, check } from 'k6';
 
 //criar já exportando a constante options - k6 vai entender onde estão as opções que estão relacionados ao teste
 export const options = {
-    iterations: 50,
+    vus: 10,
+    duration: '30s',
     thresholds: {
-        http_req_duration: ['p(90)<10', 'max<1'],
+        http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
     }
 }
