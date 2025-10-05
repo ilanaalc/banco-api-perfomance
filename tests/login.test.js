b7d712ec539bf720442fbc0289967f732de3be75
Ilana Alcantara <ilanaalcantara4@gmail.com>
2025-10-05 11:13:30 -0300

    adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -5,8 +5,13 @@ import { sleep, check } from 'k6';
 
 //criar já exportando a constante options - k6 vai entender onde estão as opções que estão relacionados ao teste
 export const options = {
-    vus: 10,
-    duration: '30s',
+    stages: [
+      { duration: '10s', target: 10 }, //durante 10s coloque 10 uv para executar os testes
+      { duration: '20s', target: 10 },
+      { duration: '10s', target: 30 },
+      { duration: '20s', target: 30 },
+      { duration: '20s', target: 0 },
+    ],
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
